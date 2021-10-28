27d68285b89959ab1479ff4520196fff4126cad9
manugonzalezzf <93330913+manugonzalezzf@users.noreply.github.com>
2021-10-28 10:38:47 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+  textAlign(CENTER,CENTER);
+  fill('#FFB6C1');
+
+
+if (keyIsDown (UP_ARROW)){
+fill('#98FB98');
+}
+text('Hello World',200,200);
+}
+
+
+
